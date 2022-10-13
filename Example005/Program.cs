9ebb9e53c808051f116fc0b11d58805b6ed19725
OlegVazhinsky <oleg_vazhinsky92@mail.ru>
2022-10-13 19:57:21 +0700

    Add Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Please enter your name -> ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "oleg") {
+    Console.WriteLine("Horay! It is Oleg!");
+} else {
+    Console.WriteLine("Hello, " + userName);
+}
